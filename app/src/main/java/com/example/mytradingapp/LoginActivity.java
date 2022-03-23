1c6350db3944e344bad6d691e214aeba9c09437e
lennartsargezian <Lennart_1997@hotmail.com>
2022-03-23 15:53:16 +0100

    Login front-end design lavet

---
+++ app/src/main/java/com/example/mytradingapp/LoginActivity.java
@@ -0,0 +1,35 @@
+package com.example.mytradingapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LoginActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_main);
+        Button button = findViewById(R.id.buttonid);
+
+    }
+
+    public void login(View view) {
+        Toast.makeText(this, "Hi User", Toast.LENGTH_LONG).show();
+
+        /*textView.setText("Yeah");
+        Log.i("test", "This is a message");*/
+
+    }
+
+
+}
