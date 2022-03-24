1a338be0951f355568c690df192fb44b92be1f5d
lennartsargezian <Lennart_1997@hotmail.com>
2022-03-24 16:30:49 +0100

    logik lavet til change themeColor

---
+++ app/src/main/java/com/example/mytradingapp/SwitchColorActivity.java
@@ -0,0 +1,56 @@
+package com.example.mytradingapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.TextView;
+
+public class SwitchColorActivity extends AppCompatActivity {
+
+    private Switch aSwitch;
+    private TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        if (AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_YES) {
+            setTheme(R.style./*darkmode*/);
+        } else {
+            setTheme(R.style./*lightmode*/);
+        }
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_switch_color);
+        aSwitch = findViewById(R.id.mode);
+        textView = findViewById(R.id.t1);
+
+        if (AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_YES) {
+            aSwitch.setChecked(true);
+        }
+
+        aSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+
+                if (isChecked) {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                    textView.setText("Dark mode");
+                } else {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                    textView.setText("Light mode");
+                    reset();
+                }
+
+            }
+        });
+    }
+
+    private void reset() {
+        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
