2ada51102d204490a44504b991a3316c19253f2d
Abdullahi Isse <abdullahi12351@gmail.com>
2022-04-03 01:21:12 +0200

    Multiple View Types til recyclerview + changesPercentage skifter farve udefra hvilken værdi den har

--- app/src/main/java/com/example/mytradingapp/View/Market/UsaFragment.java
+++ app/src/main/java/com/example/mytradingapp/View/Market/UsaFragment.java
@@ -9,17 +9,19 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.mytradingapp.R;
-import com.example.mytradingapp.Shared.Transferobjects.Post;
-import com.example.mytradingapp.Shared.Adapter.PostAdapter;
+import com.example.mytradingapp.Shared.Transferobjects.Item;
+import com.example.mytradingapp.Shared.Adapter.StockTitleAdapter;
+import com.example.mytradingapp.Shared.Transferobjects.Stock;
+import com.example.mytradingapp.Shared.Transferobjects.Title;
 
 import java.util.ArrayList;
-import java.util.List;
 
 
-public class UsaFragment extends Fragment implements PostAdapter.OnListItemClickListener {
+public class UsaFragment extends Fragment implements StockTitleAdapter.OnListItemClickListener  {
 
 
 
@@ -35,28 +37,74 @@ public class UsaFragment extends Fragment implements PostAdapter.OnListItemClick
         recyclerView.setLayoutManager(new LinearLayoutManager(inflate.getContext()));
         recyclerView.hasFixedSize();
 
-        List<Post> posts = new ArrayList<>();
+        ArrayList<Item> items = new ArrayList<>();
 
-        posts.add(new Post("AMD"));
-        posts.add(new Post("Stock1"));
-        posts.add(new Post("Stock2"));
-        posts.add(new Post("Stock"));
-        posts.add(new Post("Stock1"));
-        posts.add(new Post("Stock2"));
-        posts.add(new Post("Stock"));
-        posts.add(new Post("Stock1"));
-        posts.add(new Post("Stock2"));
+        Title title = new Title(1,"Markets Top Active");
+        items.add(new Item(0,title));
+
+        Stock stock = new Stock("F",16.65,-1.53755,"Ford Motor Company");
+
+        Stock stock1 = new Stock("F",16.65,1.53755,"Ford Motor Company");
+
+        Stock stock2 = new Stock("F",16.65,-1.53755,"Ford Motor Company");
+
+        Stock stock3 = new Stock("F",16.65,0.0,"Ford Motor Company");
+        Stock stock10 = new Stock("F",16.65,-1.53755,"Ford Motor Company");
+        Stock stock4 = new Stock("F",16.65,-1.53755,"Ford Motor Company");
+        Stock stock5 = new Stock("F",16.65,-1.53755,"Ford Motor Company");
+        Stock stock6 = new Stock("F",16.65,-1.53755,"Ford Motor Company");
+        Stock stock7 = new Stock("F",16.65,-1.53755,"Ford Motor Company");
+        Stock stock8 = new Stock("F",16.65,0.0,"Ford Motor Company");
+        Stock stock9 = new Stock("F",16.65,-1.53755,"Ford Motor Company");
+
+        items.add(new Item(1,stock));
+        items.add(new Item(1,stock1));
+        items.add(new Item(1,stock2));
+        items.add(new Item(1,stock3));
+
+        items.add(new Item(1,stock));
+        items.add(new Item(1,stock1));
+
+        Title title1 = new Title(1,"Markets Top Losers");
+        items.add(new Item(0,title1));
+
+        items.add(new Item(1,stock));
+        items.add(new Item(1,stock1));
+        items.add(new Item(1,stock2));
+        items.add(new Item(1,stock3));
+        items.add(new Item(1,stock));
+        items.add(new Item(1,stock1));
 
 
-        PostAdapter adapter = new PostAdapter(posts,this);
+        Title title2 = new Title(1,"Markets Top Gainers");
+        items.add(new Item(0,title2));
+
+        items.add(new Item(1,stock));
+        items.add(new Item(1,stock1));
+        items.add(new Item(1,stock2));
+        items.add(new Item(1,stock3));
+        items.add(new Item(1,stock));
+        items.add(new Item(1,stock1));
+
+
+
+
+        StockTitleAdapter adapter = new StockTitleAdapter(items,this);
+
+
 
         recyclerView.setAdapter(adapter);
 
         recyclerView.setAdapter(adapter);
+
+
+
+
 
         return inflate;
 
     }
+
 
     @Override
     public void onClick(int position) {
