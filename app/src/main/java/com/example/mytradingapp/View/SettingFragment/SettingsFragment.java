b936ac4eb2caebc033dbe2b77a6b808fdb40df7b
lennartsargezian <Lennart_1997@hotmail.com>
2022-05-17 19:56:40 +0200

    sign out kind of work

--- app/src/main/java/com/example/mytradingapp/View/SettingFragment/SettingsFragment.java
+++ app/src/main/java/com/example/mytradingapp/View/SettingFragment/SettingsFragment.java
@@ -1,5 +1,6 @@
 package com.example.mytradingapp.View.SettingFragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
@@ -10,8 +11,12 @@ import androidx.preference.PreferenceCategory;
 import androidx.preference.PreferenceFragmentCompat;
 
 import com.example.mytradingapp.R;
+import com.example.mytradingapp.View.Login.LoginActivity;
 import com.example.mytradingapp.View.Main.MainActivityViewModel;
 import com.example.mytradingapp.View.ReportApp.ReportFragment;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -22,6 +27,8 @@ public class SettingsFragment extends PreferenceFragmentCompat {
     private FirebaseAuth firebaseAuth;
     private MainActivityViewModel viewModel;
 
+    private GoogleSignInClient signInClient;
+
  //
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
@@ -31,9 +38,6 @@ public class SettingsFragment extends PreferenceFragmentCompat {
 
         help_categoryPreference.setOnPreferenceClickListener(this::onClickReport);
 
-        Preference signOutPreference = findPreference("signout");
-
-        signOutPreference.setOnPreferenceClickListener(this::signOut);
 
         firebaseAuth = FirebaseAuth.getInstance();
         FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
@@ -46,12 +50,28 @@ public class SettingsFragment extends PreferenceFragmentCompat {
 
         }
 
+        GoogleSignInOptions googleSignInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail().build();
+        signInClient = GoogleSignIn.getClient(getContext(), googleSignInOptions);
+
+        Preference signOutPreference = findPreference("signout");
+
+        signOutPreference.setOnPreferenceClickListener(this::signOut);
+
     }
 
+
+
     private boolean signOut(Preference preference) {
-        viewModel.signOut();
+
+        firebaseAuth.signOut();
+        signInClient.signOut();
+        startActivity(new Intent(getContext(), LoginActivity.class));
+        getActivity().finish();
         return true;
     }
+
 
 
 
